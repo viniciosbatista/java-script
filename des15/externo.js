ba8127bb826dba01c4164f2541a1f49451c5cf50
Vinicios Batista <113473637+viniciosbatista@users.noreply.github.com>
2023-09-13 23:33:53 -0300

    up
    html ok
    css ok
    js em andamento

---
+++ des15/externo.js
@@ -0,0 +1,9 @@
+var ano = new Date()
+var ano_atual = ano.getFullYear()
+var botao = document.getElementById('bt')
+botao.addEventListener('click',clicou)
+var fano = document.getElementById('ano')
+var resultado = document.getElementById('res')
+function clicou(){
+   
+}

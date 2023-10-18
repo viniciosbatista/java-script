adfc3271ec18a5bb6976cfa6d16f6f38d7fbc70b
Vinicios Batista <113473637+viniciosbatista@users.noreply.github.com>
2023-10-17 21:17:48 -0300

    up

--- index.js
+++ index.js
@@ -15,3 +15,25 @@
 //     console.log('8')
 //     console.log('10')
 // }
+
+// DESAFIO 2
+// const pi = 3.14; valor de pi
+// let raio = 10;
+// let area = Math.PI * raio * raio;
+// console.log(`a area tem ${area.toFixed(2)} m2`)
+
+// DESAFIO 3
+// let a = 7;
+// let b = 94;
+// console.log(a,b)
+// // depois da troca
+// let temp = a ;
+// a = b
+// b = temp
+// console.log(a,b) 
+
+// DESAFIO 4
+// let f = 32;
+// let g = (f -32) * 5 / 9;
+
+// console.log(`${g.toFixed(2)} °c`)

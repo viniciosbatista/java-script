85776663283e7eb5393773105138f2eb757854bc
Vinicios Batista <113473637+viniciosbatista@users.noreply.github.com>
2023-10-29 15:06:43 -0300

    up

--- index.js
+++ index.js
@@ -91,11 +91,21 @@
 // console.log('fim')
 
 // DESAFIO 10
-const nota = 10
-if (nota >=9){
-    console.log('A')
-}
-if(nota >= 7 || nota <= 8,9){
-    console.log('B')
-}
+// const nota = 10
+// if (nota >= 9 && nota <= 10){
+//     console.log('A')
+// };
+// if(nota >= 7 && nota <= 8.9){
+//     console.log('B')
+// }
+// if (nota >= 5 && nota <= 6.9){
+//     console.log('C')
+// }
+// if (nota >= 4.5 && nota <= 4.9){
+//     console.log('D')
+// }
+// if (nota >= 0 && nota < 4.5){
+//     console.log('F')
+// }
 
+// DESAFIO 11
